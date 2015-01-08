b169932f570afbf5ee4fb456eae116d4826435eb
glex86 <glexster@gmail.com>
2015-01-08 22:24:41 +0100

    remove old modifiers

--- lib/plugins/modifier.lower.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * gTemplate Engine
- * https://github.com/glex86/g-template-php
- */
-
-function tpl_modifier_lower($string) {
-    return mb_strtolower($string);
-}
--- lib/plugins/modifier.upper.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * gTemplate Engine
- * https://github.com/glex86/g-template-php
- */
-
-function tpl_modifier_upper($string) {    
-    return mb_strtoupper($string);
-}
